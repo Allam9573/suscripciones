108b6d43843d7314a1bc803b12dc5d67daf96e52
Allam9573 <aallamdaniel@gmail.com>
2024-04-02 11:49:35 -0600

    primer commit

---
+++ src/test/java/com/lukanka/app/SuscripcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukanka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuscripcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
